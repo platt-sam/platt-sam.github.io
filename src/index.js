a0e1a30452f77f4293790bd910d66f0ac857362b
Sam Platt <platts1@sou.edu>
2022-11-28 13:43:05 -0800

    create react app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {HashRouter, Routes, Route} from "react-router-dom";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <HashRouter>
+  <Routes>
+  <Route path="/" element={<App/>} />
+  </Routes>
+  </HashRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
