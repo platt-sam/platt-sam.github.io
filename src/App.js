4a2458cfd4ef7ec521bc31321f7c4f110e45757c
Sam Platt <platts1@sou.edu>
2022-11-28 11:52:02 -0800

    navbar kinda

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { AppBar, Button, Icon, IconButton, Link, Paper, Stack, styled, Toolbar} from '@mui/material';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+const pagelinks = ['linkedin.com', 'github.com'];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar>
+        <Toolbar>
+          <Button href="#" variant="text">SP</Button>
+
+          <Link href="https://linkedin.com">
+            <IconButton>
+              <LinkedInIcon></LinkedInIcon>
+            </IconButton>
+          </Link>
+
+          <Link href="https://github.com">
+            <IconButton>
+              <GitHubIcon></GitHubIcon>
+            </IconButton>
+          </Link>
+          
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
