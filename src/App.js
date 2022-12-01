e8c524db33b07ce6acdd6ad60a758382586e847e
Sam Platt <platts1@sou.edu>
2022-11-30 19:49:57 -0800

    new formatting and revising project descriptions

--- src/App.js
+++ src/App.js
@@ -33,13 +33,40 @@ function App() {
 
       <div className="main">
 
-        <Grid container columns={3} spacing={"25px"} sx={{ marginTop: "12.5px" }} >
+        <Grid container columns={6} spacing={"25px"} >
 
-          <Grid item xs={1}> 
+          <Grid item xs={6} sm={3} md={2}>
             <Card className="project" >
-                <CardHeader title="Conversational Transcript" subheader="aws, awstranscribe, python, reactjs" />
-                <CardContent>
-                Conversational Transcription is a web application created for the Southern Oregon University IT department. Dynamically generates transcripts of conversations with multiple audio input sources. Transcripts specify the speaker and includes timestamps for each piece of conversation.
+                <CardContent className="projectname">
+                  Conversational Transcript 2.0
+                </CardContent>
+
+                <CardContent className="projectskills">
+                  aws, project management, python, reactjs
+                </CardContent>
+
+                <CardContent className="projectpreview">
+                  Web application created for the Southern Oregon University IT department. Dynamically generates transcripts of conversations with multiple audio input sources. Transcripts specify the speaker and includes timestamps for each piece of conversation. Developed with Liam Erickson and Joshua Yoon.
+                </CardContent>
+
+                <CardActions className="actions" >
+                  <Link href="#" >+ see more</Link>
+                </CardActions>
+            </Card>
+          </Grid>
+
+          <Grid item xs={6} sm={3} md={2}>
+            <Card className="project">
+                <CardContent className="projectname">
+                  Laughing Stock
+                </CardContent>
+
+                <CardContent className="projectskills">
+                  game design, html, java, project management
+                </CardContent>
+
+                <CardContent className="projectpreview">
+                  Choose your own adventure game where the player attempts to board a plane and ends up landing on an island filled with clowns. First place winner of the HackOR 2021 Hackathon. Developed with Liam Erickson, Ronin Ganoot, and Peter Jacobson.
                 </CardContent>
                 <CardActions className="actions" >
                   <Link href="#" >+ see more</Link>
@@ -47,11 +74,18 @@ function App() {
             </Card>
           </Grid>
 
-          <Grid item xs={1}> 
-          <Card className="project">
-                <CardHeader title="Laughing Stock" subheader="gamedesign, html, java" />
-                <CardContent>
-                  Lorem ipsum dolor sit amet. Consectetur adipiscing elit.
+          <Grid item xs={6} sm={3} md={2}>
+            <Card className="project" >
+                <CardContent className="projectname">
+                  Medford Charities and Social Services
+                </CardContent>
+
+                <CardContent className="projectskills">
+                  css, docker, html, gcp, google maps api, python
+                </CardContent>
+
+                <CardContent className="projectpreview">
+                  Google Cloud Platform app displaying information about charities and social services in the Medford, Oregon area. Allows user submitted entries. Uses the Google Maps API to show site locations.
                 </CardContent>
                 <CardActions className="actions" >
                   <Link href="#" >+ see more</Link>
@@ -59,50 +93,85 @@ function App() {
             </Card>
           </Grid>
 
-          <Grid item xs={1}> 
+          <Grid item xs={6} sm={3} md={2}>  
             <Card className="project" >
-                <CardHeader title="Medford Charities and Social Services" subheader="gcp, googlemapsapi, python" />
-                <CardContent>
-                  Google Cloud Platform (GCP) app displaying information about charities and social services in the Medford, Oregon area. Allows user submitted entries. Written in Python using Flask. Uses the Google Maps API to show site locations.
+                <CardContent className="projectname">
+                  Ballad of the Woods
                 </CardContent>
+                
+                <CardContent className="projectskills">
+                  css, html, javascript, game design
+                </CardContent>
+
+                <CardContent className="projectpreview">
+                  A browser based videogame loosely inspired by the musical <i>Hadestown</i>. Developed with Gianna McCardell and Dean Wilson.
+                </CardContent>
+
                 <CardActions className="actions" >
                   <Link href="#" >+ see more</Link>
                 </CardActions>
             </Card>
           </Grid>
 
-          <Grid item xs={1}> 
+          <Grid item xs={6} sm={3} md={2}> 
             <Card className="project" >
-                <CardHeader title="Ballad of the Woods" subheader="html, javasript, gamedesign" />
-                <CardContent>
-                  A browser based videogame loosely inspired by the musical “Hadestown”. Browser based game written in JavaScript with some HTML and CSS
+                <CardContent className="projectname">
+                  Recipe Substitutor
                 </CardContent>
-                <CardActions className="actions" >
-                  <Link href="#" >+ see more</Link>
-                </CardActions>
-            </Card>
-          </Grid>
 
-          <Grid item xs={1}> 
-            <Card className="project" >
-                <CardHeader title="Recipe Substituor" subheader="python" />
-                <CardContent>
+                <CardContent className="projectskills">
+                  data scrubbing, python
+                </CardContent>
+
+                <CardContent className="projectpreview">
                   Program that generates comments suggesting that a random ingredient from a given ingredient list is substituted with one randomly chosen from a list of thousands of ingredients.
                 </CardContent>
+
                 <CardActions className="actions" >
                   <Link href="#" >+ see more</Link>
                 </CardActions>
             </Card>
           </Grid>
 
-          <Grid item xs={1}> 
+          <Grid item xs={6} sm={3} md={2}>
             <Card className="project" >
-                <CardHeader title="Computer Science Club Poster" subheader="adobeillustrator, graphicdesign, marketing, typography" />
-                <CardContent>
-                Poster created to advertise Southern Oregon University's Computer Science club, highlighting several key events to take place during the 2022-23 school year.
+                <CardContent className="projectname">
+                  Docker Access Management
                 </CardContent>
+
+                <CardContent className="projectskills">
+                  docker, go, reactjs
+                </CardContent>
+
+                <CardContent className="projectpreview">
+                  Unpublished Docker extension created for an internal hackathon. Allows management of Docker organizations, groups, and images from Docker Desktop. Developed with Victor Bersy, Jonathan Dieu, Sylvialynn Favello, and Ryan Hristovski.
+                </CardContent>
+
                 <CardActions className="actions" >
-                  <Link href="https://drive.google.com/file/d/1UoewJIciUST3yoQMVQTvgrjl-Ip_Zbzm/view?usp=share_link" >+ see more</Link>
+                  <Link href="#" >+ see more</Link>
+                </CardActions>
+            </Card>
+          </Grid>
+
+          <Grid item xs={6} sm={3} md={2}>
+            <Card className="project">
+                <CardContent className="projectname">
+                  Computer Science Club Poster
+                </CardContent>
+
+                <CardContent className="projectskills">
+                  adobe illustrator, graphic design, marketing, typography
+                </CardContent>
+
+                <CardMedia
+                  component="img"
+                  image="https://drive.google.com/uc?id=1UoewJIciUST3yoQMVQTvgrjl-Ip_Zbzm"
+                  alt="southern oregon university computer science club poster"
+                  sx={{objectFit: "contain"}}
+                />
+
+                <CardActions className="actions" >
+                  <Link href="https://drive.google.com/file/d/1UoewJIciUST3yoQMVQTvgrjl-Ip_Zbzm/view">+ see more</Link>
                 </CardActions>
             </Card>
           </Grid>
